64453e12da0d24c611c9da10a36a5143d12a68bd
JorgeMBP <jorge@carretie.com>
2015-06-30 17:36:23 +0200

    trustly require

--- lib/trustly.rb
+++ lib/trustly.rb
@@ -1,3 +1,6 @@
+module Trustly
+end
+
 require "trustly/exception/authentification_error"
 require "trustly/exception/connection_error"
 require "trustly/exception/data_error"
@@ -5,9 +8,12 @@ require "trustly/exception/jsonrpc_version_error"
 require "trustly/exception/signature_error"
 
 require "trustly/data"
+require "trustly/data/jsonrpc_request"
+require "trustly/data/jsonrpc_response"
+require "trustly/data/jsonrpcnotificationrequest"
+require "trustly/data/jsonrpcnotificationrequest"
+
 require "trustly/api"
 require "trustly/api/signed"
 require "trustly/version"
 
-module Trustly
-end
