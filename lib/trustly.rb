c091e10cadffcdc7b3a541c2c881b47919f8fab5
JorgeMBP <jorge@carretie.com>
2015-06-30 17:34:25 +0200

    trustly require

--- lib/trustly.rb
+++ lib/trustly.rb
@@ -1,6 +1,12 @@
-require "trustly/exception"
+require "trustly/exception/authentification_error"
+require "trustly/exception/connection_error"
+require "trustly/exception/data_error"
+require "trustly/exception/jsonrpc_version_error"
+require "trustly/exception/signature_error"
+
 require "trustly/data"
 require "trustly/api"
+require "trustly/api/signed"
 require "trustly/version"
 
 module Trustly
